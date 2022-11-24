50d15bddc7e2207cf40ad4d84ee272567cdfb898
Irina <irishkasvist@mail.ru>
2022-11-24 21:47:15 +0300

    dz_6

---
+++ CounterPositiveNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿void HandFillArray(double[] array, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        Console.Write("Введите " + (i + 1) + " число: ");
+        array[i] = Convert.ToDouble(Console.ReadLine()!);
+    }
+}
+void PrintArray(double[] array)
+{
+    var str = string.Join(" ,", array);
+    Console.Write("Введены числа: ");
+    Console.WriteLine("[" + str + "]");
+}
+int CounterPositiveNumbers(double[] array, int n)
+{
+    int count = 0;
+    for (int i = 0; i < n; i++) if (array[i] > 0) count++;
+    return count;
+}
+Console.WriteLine("Введите количество чисел для проверки (М): ");
+int m = int.Parse(Console.ReadLine()!);
+double[] newarray = new double[m];
+HandFillArray(newarray, m);
+PrintArray(newarray);
+Console.WriteLine("Количество введенных положительных чисел: " + CounterPositiveNumbers(newarray, m));
+
+
---
+++ LineCrossPoint/Program.cs
@@ -0,0 +1,33 @@
+﻿void HandFill(double[] array, int n)
+{
+    Console.Write("Введите b" + n + ": ");
+    array[1] = Convert.ToDouble(Console.ReadLine()!);
+    Console.Write("Введите k" + n + ": ");
+    array[0] = Convert.ToDouble(Console.ReadLine()!);
+}
+void PrintArray(double[] array)
+{
+    var str = string.Join(" ,", array);
+    Console.WriteLine("[" + str + "]");
+}
+double[] LineCrossPoint(double[] array1, double[] array2)
+{
+    double[] crosspoint = new double[2];
+    crosspoint[0] = Math.Round(-1 * (array2[1] - array1[1]) / (array2[0] - array1[0]), 2);
+    crosspoint[1] = Math.Round(-1 * (array1[0] * array2[1] - array2[0] * array1[1]) / (array2[0] - array1[0]), 2);
+    return crosspoint;
+}
+
+Console.WriteLine("Введите коэфaициенты уравнения прямой y=k1*x + b1");
+double[] line1 = new double[2];
+HandFill(line1, 1);
+Console.WriteLine("Введите коэфaициенты уравнения прямой y=k2*x + b2");
+double[] line2 = new double[2];
+HandFill(line2, 2);
+if (line2[0] - line1[0] == 0) Console.WriteLine("Введенные прямые параллельны");
+else
+{
+    double[] crosspoint = LineCrossPoint(line1, line2);
+    Console.Write("Координаты точки пересечения: ");
+    PrintArray(crosspoint);
+}
